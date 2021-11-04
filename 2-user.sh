420c139e2435b516921f713a2f997b0bc4f5eda3
Pascal2907Git <pascal2907fs@numericable.fr>
2021-11-04 15:23:16 +0100

    supp2

--- 2-user.sh
+++ 2-user.sh
@@ -26,13 +26,9 @@ PKGS=(
 'timeshift'
 'autojump'
 'awesome-terminal-fonts'
-#'brave-bin' # Brave Browser
-'dxvk-bin' # DXVK DirectX to Vulcan
 'github-desktop-bin' # Github Desktop sync
 'lightly-git'
 'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
 'nerd-fonts-fira-code'
 'nordic-darker-standard-buttons-theme'
 'nordic-darker-theme'
@@ -43,13 +39,10 @@ PKGS=(
 'plasma-pa'
 'ocs-url' # install packages from websites
 'sddm-nordic-theme-git'
-#'snapper-gui-git'
 'ttf-droid'
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
 'ttf-roboto'
-'zoom' # video conferences
-'snap-pac'
 )
 
 for PKG in "${PKGS[@]}"; do
